6d66f0d2a102ad553de2a4e486c85f715e7c7738
alejandro97ord <59874702+alejandro97ord@users.noreply.github.com>
2020-10-22 12:13:45 -0600

    code update

--- code/compKern.m
+++
@@ -1,8 +0,0 @@
-function K=compKern(x1,x2,hyp)
-
-K=zeros(size(x1,2),size(x2,2));
-for i=1:size(x1,2)
-    K(i,:)=hyp(1)^2*exp(-sum((x1(1:end-1,i)-x2(1:end-1,:)).^2,1)./(2*hyp(2)^2))+x1(end,i)*hyp(3)^2*exp(-sum((x1(1:end-1,i)-x2(1:end-1,:)).^2,1)./(2*hyp(4)^2)).*x2(end,:);
-end
-
-end
--- code/loglikmax.m
+++
@@ -1,7 +0,0 @@
-function hyp=loglikmax(kernel,hyp0,X,y,sn2)
-
-ac=@(hyp)y*((kernel(X,X,hyp)+sn2*eye(size(X,2)))\y')+2*sum(log(diag(chol(kernel(X,X,hyp)+sn2*eye(size(X,2))))));
-
-hyp=fmincon(ac,hyp0,[],[],[],[],ones(size(hyp0))*0.1,ones(size(hyp0))*100);
-
-end
